8430f1b7ecc5372a3a8c33c54353e370a8bc68c5
Nasir Uddin <nasiruddin3954@gmail.com>
2025-08-08 22:55:53 +0600

    xpsc

---
+++ Leave_no_Witnesses.cpp
@@ -0,0 +1,5 @@
+//https://www.codechef.com/problems/DISPART
+
+//  Leave no Withnesses (কোন সাক্ষীকে রাখবেন না)
+//Let B be a binary string of length N.
+//The integer X is said to be a withness for B if the following condition holds :
--- Restricted_Removals.cpp
+++ Restricted_Removals.cpp
@@ -24,15 +24,115 @@
 // array A and B , index is 2 , two elements are aligned, possible option 1
 // array A and B, index is 5, two elements are aligned , possible option 2
 // From array A index 5 to last index, going to be shifted anyways
-//From array A index 2 to 5, only be shifted if 1 is popped
+// From array A index 2 to 5, only be shifted if 1 is popped
 
-//Pop the last position i where a[i] == b[i]
-//First, do this repeatedly until we can't. print the size of A.
+// Pop the last position i where a[i] == b[i]
+// First, do this repeatedly until we can't. print the size of A.
 
-//Expected Time Complexity  : 0(N)
-//Approach : greedily do the last alignment fist, later the earlier alignments
+// Expected Time Complexity  : 0(N)
+// Approach : greedily do the last alignment fist, later the earlier alignments
 
-//How to do this in 0(n) or 0(nlogn) ?
-//For a given a[i] to be popped
-//  -> if (a[i] == 1) -> we need to find the earliest previous b[i] = 1.
-//  -> 
+// How to do this in 0(n) or 0(nlogn) ?
+// For a given a[i] to be popped
+//   -> if (a[i] == 1) -> we need to find the earliest previous b[j] = 1.
+//   -> if (a[i] == 0) -> we need to find the earliest previous b[j] = 0
+//  how much "distance" to the previous  equal value
+
+// Algorithm :
+//  For each value i, (i <= n)
+//   find the previous position j such that b[j] == a[i]
+//  Prefix array approach
+// For each value i,
+// distance = i - prev[i]
+// if(deletion >= distance){
+//  we can reach and make a new deletion. we can just
+// deletion ++;
+//}
+// ans = N - deletion
+
+// some brief idea of an alternate approach
+//  a = [0,.........]       a = [1,.........]
+//  b = [0,1]               b = [1,0,.......]
+// after this pattern -> entire of array A can be deleted
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        vector<int> a(n);
+        for (auto &x : a)
+            cin >> x;
+
+        vector<int> b(m);
+        for (auto &x : b)
+            cin >> x;
+
+        vector<int> prev_0(n, 1e9), prev_1(m, 1e9);
+        for (int i = 0; i < n; i++)
+        {
+            if (i)
+                prev_0[i] = prev_0[i - 1] + 1;
+            if (i)
+                prev_1[i] = prev_1[i - 1] + 1;
+            if (i < m && b[i] == 0)
+                prev_0[i] = 0;
+            if (i < m && b[i] == 1)
+                prev_1[i] = 0;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == 0 && prev_0[i] <= ans)
+                ans++;
+            if (a[i] == 1 && prev_1[i] <= ans)
+                ans++;
+        }
+
+        cout << n - ans << endl;
+    }
+
+    return 0;
+}
+
+
+Initialization
+INF = 1000000000
+prev_0 = [INF, INF, INF]
+prev_1 = [INF, INF, INF]
+
+Loop i = 0:
+
+i == 0 so we skip i-dependent increment.
+i < m and b[0] == 0 → set prev_0[0] = 0.
+b[0] is not 1 so prev_1[0] remains INF.
+Now: prev_0 = [0, INF, INF], prev_1 = [INF, INF, INF].
+
+Loop i = 1:
+prev_0[1] = prev_0[0] + 1 = 0 + 1 = 1.
+prev_1[1] = prev_1[0] + 1 = INF + 1 = INF + 1 (still effectively INF).
+i < m and b[1] == 1 → set prev_1[1] = 0.
+Now: prev_0 = [0, 1, INF], prev_1 = [INF, 0, INF].
+
+Loop i = 2:
+prev_0[2] = prev_0[1] + 1 = 1 + 1 = 2.
+prev_1[2] = prev_1[1] + 1 = 0 + 1 = 1.
+i < m and b[2] == 0 → set prev_0[2] = 0.
+Now: prev_0 = [0, 1, 0], prev_1 = [INF, 0, 1].
+
+Second pass: ans = 0
+
+i = 0: a[0] == 0 and prev_0[0] == 0 → 0 <= 0 true → ans = 1
+i = 1: a[1] == 1 and prev_1[1] == 0 → 0 <= 1 true → ans = 2
+i = 2: a[2] == 1 and prev_1[2] == 1 → 1 <= 2 true → ans = 3
+
+Result printed: n - ans = 3 - 3 = 0.
+
+So in this example every element of a got matched → zero left unmatched.
